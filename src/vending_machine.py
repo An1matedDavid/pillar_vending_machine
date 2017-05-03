b702dd2d458423fc3e8d28088087598e2123207a
David Carroll <dc113604@ohio.edu>
2017-05-03 17:54:11 -0400

    Test AcceptCoins with coin object

--- src/vending_machine.py
+++ src/vending_machine.py
@@ -3,6 +3,7 @@ __author__ = 'MrJ'
 class VendingMachine:
     def __int__(self):
         self.coins = []
+        self.display = "INSERT COIN"
 
     def accept_coins(self, coin):
         return coin
--- tests/accept_coins_test.py
+++ tests/accept_coins_test.py
@@ -2,15 +2,26 @@ __author__ = 'David Carroll'
 import unittest
 from src.vending_machine import VendingMachine
 
+# size = mm diameter
+# weight = g
+quarter = {'weight': 5.6, 'size': 24.2}
+dime = {'weight': 2.2, 'size': 17.9}
+nickel = {'weight': 5, 'size': 21.2}
+penny = {'weight': 2.5, 'size': 19}
 
 class TestAcceptCoins(unittest.TestCase):
     # def __int__(self):
     #     self.VendingMachine = VendingMachine()
 
-    def test_when_vending_machine_is_passed_a_coin_it_returns_that_coin(self):
+    def test_when_vending_machine_is_passed_a_coin_value_it_returns_that_value(self):
         self.VendingMachine = VendingMachine()
         coin = 5
         self.assertEqual(self.VendingMachine.accept_coins(coin), 5)
+
+    def test_when_vending_machine_is_passed_a_coin_it_returns_that_coin(self):
+        self.VendingMachine = VendingMachine()
+        coin = quarter
+        self.assertEqual(self.VendingMachine.accept_coins(coin), quarter)
 
 # AcceptCoins = TestAcceptCoins()
 # AcceptCoins.test_when_vending_machine_is_passed_a_coin_it_evaluates_that_coin()
