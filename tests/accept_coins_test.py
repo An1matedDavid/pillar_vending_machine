720329157644aa90519746807291f1929e6c2eb9
David Carroll <dc113604@ohio.edu>
2017-05-03 17:57:06 -0400

    cleaned up test, made VendingMachine a class property

--- tests/accept_coins_test.py
+++ tests/accept_coins_test.py
@@ -10,21 +10,16 @@ nickel = {'weight': 5, 'size': 21.2}
 penny = {'weight': 2.5, 'size': 19}
 
 class TestAcceptCoins(unittest.TestCase):
-    # def __int__(self):
-    #     self.VendingMachine = VendingMachine()
+
+    VendingMachine = VendingMachine()
 
     def test_when_vending_machine_is_passed_a_coin_value_it_returns_that_value(self):
-        self.VendingMachine = VendingMachine()
         coin = 5
         self.assertEqual(self.VendingMachine.accept_coins(coin), 5)
 
     def test_when_vending_machine_is_passed_a_coin_it_returns_that_coin(self):
-        self.VendingMachine = VendingMachine()
         coin = quarter
         self.assertEqual(self.VendingMachine.accept_coins(coin), quarter)
-
-# AcceptCoins = TestAcceptCoins()
-# AcceptCoins.test_when_vending_machine_is_passed_a_coin_it_evaluates_that_coin()
 
 if __name__ == '__main__':
     unittest.main()
