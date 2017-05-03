5c2d4bc3b8b3604d312391bc0a5ee342dc4a82bc
David Carroll <dc113604@ohio.edu>
2017-05-03 19:52:00 -0400

    New test: test_when_vending_machine_is_passed_a_coin_it_updates_current_amount_or_returns_invalid_coin. First test with coin value deleted, no longer functions due to Coin dict object.

--- src/vending_machine.py
+++ src/vending_machine.py
@@ -1,9 +1,45 @@
 __author__ = 'MrJ'
 
+
 class VendingMachine:
-    def __int__(self):
-        self.coins = []
+
+    current_amount = 0
+    coin_return = []
+    display = "INSERT COIN"
+
+    def vending_machine_reset(self):
+        self.current_amount = 0
+        self.coin_return = []
         self.display = "INSERT COIN"
 
     def accept_coins(self, coin):
+        is_valid = self.is_valid_coin(coin)
+        if is_valid is True:
+            self.add_coin_value_to_current_amount(coin)
+        else:
+            self.coin_return.append(coin)
         return coin
+
+    def is_valid_coin(self, coin):
+        valid_coin = False
+        # quarter
+        if coin['weight'] == 5.6 and coin['size'] == 24.2:
+            valid_coin = True
+        # dime
+        elif coin['weight'] == 2.2 and coin['size'] == 17.9:
+            valid_coin = True
+        # nickel
+        elif coin['weight'] == 5 and coin['size'] == 21.2:
+            valid_coin = True
+        return valid_coin
+
+    def add_coin_value_to_current_amount(self, coin):
+        # quarter
+        if coin['weight'] == 5.6 and coin['size'] == 24.2:
+            self.current_amount += 25
+        # dime
+        elif coin['weight'] == 2.2 and coin['size'] == 17.9:
+            self.current_amount += 10
+        # nickel
+        elif coin['weight'] == 5 and coin['size'] == 21.2:
+            self.current_amount += 5
--- tests/accept_coins_test.py
+++ tests/accept_coins_test.py
@@ -9,17 +9,31 @@ dime = {'weight': 2.2, 'size': 17.9}
 nickel = {'weight': 5, 'size': 21.2}
 penny = {'weight': 2.5, 'size': 19}
 
+
 class TestAcceptCoins(unittest.TestCase):
 
     VendingMachine = VendingMachine()
 
-    def test_when_vending_machine_is_passed_a_coin_value_it_returns_that_value(self):
-        coin = 5
-        self.assertEqual(self.VendingMachine.accept_coins(coin), 5)
-
     def test_when_vending_machine_is_passed_a_coin_it_returns_that_coin(self):
         coin = quarter
         self.assertEqual(self.VendingMachine.accept_coins(coin), quarter)
+        self.VendingMachine.vending_machine_reset()
+
+    def test_when_vending_machine_is_passed_a_coin_it_updates_current_amount_or_returns_invalid_coin(self):
+        coin1 = quarter
+        self.VendingMachine.accept_coins(coin1)
+        self.assertEqual(self.VendingMachine.current_amount, 25)
+        self.assertEqual(self.VendingMachine.coin_return, [])
+
+        coin2 = penny
+        self.VendingMachine.accept_coins(coin2)
+        self.assertEqual(self.VendingMachine.current_amount, 25)
+        self.assertEqual(self.VendingMachine.coin_return, [{'size': 19, 'weight': 2.5}])
+
+        coin3 = dime
+        self.VendingMachine.accept_coins(coin3)
+        self.assertEqual(self.VendingMachine.current_amount, 35)
+        self.assertEqual(self.VendingMachine.coin_return, [{'size': 19, 'weight': 2.5}])
 
 if __name__ == '__main__':
     unittest.main()
