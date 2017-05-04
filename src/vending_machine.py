e93fa580b21f3dcd7fafaf42a268e02ed270f5f8
David Carroll <dc113604@ohio.edu>
2017-05-04 12:31:54 -0400

    test_check_transaction_for_correct_balance improved by using an internal balance property in VendingMachine. Previous accept_coin test split up so each test has single responsibility

--- src/vending_machine.py
+++ src/vending_machine.py
@@ -7,12 +7,14 @@ class VendingMachine:
     coin_return = []
     display = "INSERT COIN"
     price = 0
+    balance = 0
 
     def vending_machine_reset(self):
         self.current_amount = 0
         self.coin_return = []
         self.display = "INSERT COIN"
         self.price = 0
+        self.balance = 0
 
     def accept_coins(self, coin):
         is_valid = self.is_valid_coin(coin)
@@ -38,13 +40,13 @@ class VendingMachine:
     def add_coin_value_to_current_amount(self, coin):
         # quarter
         if coin['weight'] == 5.6 and coin['size'] == 24.2:
-            self.current_amount += 25
+            self.current_amount += 0.25
         # dime
         elif coin['weight'] == 2.2 and coin['size'] == 17.9:
-            self.current_amount += 10
+            self.current_amount += 0.10
         # nickel
         elif coin['weight'] == 5 and coin['size'] == 21.2:
-            self.current_amount += 5
+            self.current_amount += 0.05
 
     def cola_button_press(self):
         self.price = 1.00
@@ -56,11 +58,9 @@ class VendingMachine:
         self.price = 0.65
 
     def _check_transaction(self):
-        balance = self.price - self.current_amount
+        self.balance = self.price - self.current_amount
         # if balance < price, do "need more money" things
 
         # if balance == price, dispence and end transaction.
 
-        # if balacne > price, do "make change" things.
-
-        return balance
+        # if balance > price, do "make change" things.
--- tests/accept_coins_test.py
+++ tests/accept_coins_test.py
@@ -19,20 +19,21 @@ class TestAcceptCoins(unittest.TestCase):
         self.assertEqual(self.VendingMachine.accept_coins(coin), quarter)
         self.VendingMachine.vending_machine_reset()
 
-    def test_when_vending_machine_is_passed_a_coin_it_updates_current_amount_or_returns_invalid_coin(self):
+    def test_when_vending_machine_is_passed_a_valid_coin_it_updates_current_amount(self):
         coin1 = quarter
         self.VendingMachine.accept_coins(coin1)
-        self.assertEqual(self.VendingMachine.current_amount, 25)
+        self.assertEqual(self.VendingMachine.current_amount, 0.25)
         self.assertEqual(self.VendingMachine.coin_return, [])
-        coin2 = penny
-        self.VendingMachine.accept_coins(coin2)
-        self.assertEqual(self.VendingMachine.current_amount, 25)
-        self.assertEqual(self.VendingMachine.coin_return, [{'size': 19, 'weight': 2.5}])
         coin3 = dime
         self.VendingMachine.accept_coins(coin3)
-        self.assertEqual(self.VendingMachine.current_amount, 35)
+        self.assertEqual(self.VendingMachine.current_amount, 0.35)
+        self.assertEqual(self.VendingMachine.coin_return, [])
+
+    def test_when_vending_machine_is_passed_an_invalid_coin_it_is_added_to_return(self):
+        coin2 = penny
+        self.VendingMachine.accept_coins(coin2)
+        self.assertEqual(self.VendingMachine.current_amount, 0.35)
         self.assertEqual(self.VendingMachine.coin_return, [{'size': 19, 'weight': 2.5}])
-        # self.VendingMachine.vending_machine_reset()
 
     def test_when_product_is_selected_price_is_set(self):
         self.VendingMachine.cola_button_press()
@@ -43,7 +44,11 @@ class TestAcceptCoins(unittest.TestCase):
         self.assertEqual(self.VendingMachine.price, 0.65)
 
     def test_check_transaction_for_correct_balance(self):
-        balance = self.VendingMachine._check_transaction()
+        coin = quarter
+        self.VendingMachine.accept_coins(coin)
+        self.VendingMachine.cola_button_press()
+        self.VendingMachine._check_transaction()
+        self.assertEqual(self.VendingMachine.balance, 0.75)
 
 if __name__ == '__main__':
     unittest.main()
